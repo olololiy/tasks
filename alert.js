422470f8f72df1eb333752690bb28cccee3da1a4
olololiy <olololiy@bk.ru>
2021-08-17 11:12:08 +0300

    Задача на циклы

--- alert.js
+++ alert.js
@@ -1,7 +1,24 @@
-let result;
+// for (let i = 0; i < 3; i++) {
+//   alert( `number ${i}!` );
+// }
 
-if (a + b < 4) {
-  result = 'Мало';
-} else {
-  result = 'Много';
+let maxNum;
+let currentNum = 2;
+let divNum = 2;
+let stringNum ;
+
+maxNum = prompt('Введите число больше 1');
+while ( currentNum <= maxNum ){
+    if(currentNum % divNum != 0){
+      divNum++;
+    }
+    else{
+      if(currentNum == divNum){
+        //записываем в с stringNumString(value)
+        //stringNum = stringNum + String(currentNum);
+        alert(currentNum);
+      }
+      divNum = 2;
+      currentNum++;
+    }
 }
